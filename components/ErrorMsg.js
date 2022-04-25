9b789e3768c494ff260fd86ddb2301745c067e65
orindholt <U75087@rts-365.dk>
2022-04-25 14:13:36 +0200

    Modify error message

--- components/ErrorMsg.js
+++ components/ErrorMsg.js
@@ -1,14 +1,7 @@
-const ErrorMsg = ({name, error}) => {
-	const nameRegEx = /([A-Z])+/g;
-	if (error === "required") error = "is required!";
-	if (nameRegEx.test(name))
-		name = name.replace(
-			name.match(nameRegEx).toString(),
-			` ${name.match(nameRegEx).toString().toLowerCase()}`
-		);
+const ErrorMsg = ({message}) => {
 	return (
 		<p className="text-red-500 absolute -top-8 w-full text-center">
-			{`${name} ${error}`}
+			{`${message}`}
 		</p>
 	);
 };
