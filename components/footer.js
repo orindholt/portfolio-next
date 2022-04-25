df5b3fc424908f62e0d2903513aa7f180737d736
orindholt <U75087@rts-365.dk>
2022-04-25 14:14:02 +0200

    Remove mobile nav from footer

--- components/footer.js
+++ components/footer.js
@@ -1,19 +1,11 @@
 import Socials from "./Socials";
 import Link from "next/link";
-import {IoHome} from "react-icons/io5";
 
 const Footer = () => {
 	return (
-		<footer className="mt-auto">
+		<footer className="mt-auto hidden md:block">
 			<div className="md:flex hidden dark:bg-white bg-black w-full py-2 md:justify-center dark:text-black text-white">
 				<Socials />
-			</div>
-			<div className="w-full flex justify-evenly fixed bottom-0 text-5xl py-2 md:hidden dark:bg-black bg-white bg-opacity-80 backdrop-blur-sm">
-				<Link href="/">
-					<div className="active:scale-90 transition-transform">
-						<IoHome />
-					</div>
-				</Link>
 			</div>
 		</footer>
 	);
