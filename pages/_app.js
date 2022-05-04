7f3a592ad89d17e6d6a0414ac266bef1ffe788cd
orindholt <U75087@rts-365.dk>
2022-05-04 11:48:07 +0200

    Use context provider component instead of hook

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import {ThemeProvider} from "next-themes";
+import FormContextProvider from "../Utility/Context";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 
 function MyApp({Component, pageProps}) {
 	return (
-		<ThemeProvider attribute="class">
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</ThemeProvider>
+		<FormContextProvider>
+			<ThemeProvider attribute="class">
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</ThemeProvider>
+		</FormContextProvider>
 	);
 }
 
