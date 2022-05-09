a3faed6137a18e1c5a1a30dfbd516e69ac7d0bc3
Oliver Rindholt <U75087@rts-365.dk>
2022-05-10 01:37:57 +0200

    Add data to career tree components

--- components/CareerTree/Tree.js
+++ components/CareerTree/Tree.js
@@ -1,19 +1,55 @@
-import {motion as m} from "framer-motion";
-import {IoLogoLinkedin} from "react-icons/io5";
+import { motion as m } from "framer-motion";
+import { IoLogoLinkedin } from "react-icons/io5";
 import CareerBranch from "./Branch";
 
-const positions = [{name: "", date: "", desc: ""}];
+const date = new Date();
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
+];
+
+const positions = [
+	{
+		name: "3 Wise Monkeys",
+		status: "Junior Frontend Developer",
+		date: {
+			from: { year: "2022", month: "April" },
+			to: { year: date.getFullYear(), month: months[date.getMonth()] },
+		},
+		desc: "",
+	},
+	{
+		name: "3 Wise Monkeys",
+		status: "Junior Frontend Developer",
+		date: {
+			from: { year: "2022", month: "April" },
+			to: { year: date.getFullYear(), month: months[date.getMonth()] },
+		},
+		desc: "",
+	},
+];
 
 const CareerTree = () => {
 	return (
-		<>
-			<div>
-				{positions.map((i, position) => {
+		<div className="my-6 flex flex-col">
+			<h2 className="font-light text-3xl text-left my-4">Where have I been?</h2>
+			<ul className="flex flex-col gap-4">
+				{positions.map((position, i) => {
 					return <CareerBranch key={i} position={position} />;
 				})}
-			</div>
+			</ul>
 			<a
-				className="gap-1 items-center md:hover:text-orange text-xl inline-flex mr-auto"
+				className="gap-1 items-center md:hover:text-orange text-xl inline-flex mr-auto mt-4 font-medium"
 				target="_blank"
 				rel="noreferrer"
 				href="https://www.linkedin.com/in/oliver-rindholt-55707b217"
@@ -21,9 +57,9 @@ const CareerTree = () => {
 				<span>
 					<IoLogoLinkedin />
 				</span>
-				See more
+				LinkedIn
 			</a>
-		</>
+		</div>
 	);
 };
 
