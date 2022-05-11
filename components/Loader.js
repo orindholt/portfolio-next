3de250a6f0b95da938ed842d2c1c269bb8613ade
Oliver Rindholt <U75087@rts-365.dk>
2022-05-11 04:01:40 +0200

    Add styling to loader

--- components/Loader.js
+++ components/Loader.js
@@ -4,6 +4,7 @@ import { IoSync } from "react-icons/io5";
 const Loader = () => {
 	return (
 		<m.div
+			className="text-4xl text-silver inline-block"
 			initial={{ rotate: 0 }}
 			animate={{ rotate: 360 }}
 			transition={{ repeat: Infinity, duration: 1 }}
