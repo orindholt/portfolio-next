8326277d336069ad0439d7ce01622da1e1d59168
orindholt <u75087@rts-365.dk>
2022-05-26 01:47:44 +0200

    Add tech cards to about page

--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,99 @@
 import Head from "next/head";
 import Image from "next/image";
 import TechCard from "../components/TechCard";
-import { IoLogoReact } from "react-icons/io5";
+import {
+	IoLogoReact,
+	IoLogoHtml5,
+	IoLogoCss3,
+	IoLogoJavascript,
+	IoLogoSass,
+	IoLogoGithub,
+} from "react-icons/io5";
+import { SiNextdotjs, SiTailwindcss, SiAdobephotoshop } from "react-icons/si";
 
 const expTech = [
 	{
+		name: "HTML5",
+		nameColor: "white",
+		bg: "#FF5733",
+		icon: <IoLogoHtml5 />,
+		iconColor: "white",
+		gradientFrom: "#FF5733",
+		levelPercent: 100,
+	},
+	{
+		name: "CSS3",
+		nameColor: "white",
+		bg: "#2196f3",
+		icon: <IoLogoCss3 />,
+		iconColor: "white",
+		gradientFrom: "#2196f3",
+		levelPercent: 90,
+	},
+	{
+		name: "Tailwind",
+		nameColor: "white",
+		bg: "#0bb6d4",
+		icon: <SiTailwindcss />,
+		iconColor: "white",
+		gradientFrom: "#0bb6d4",
+		gradientTo: "#1dc9b9",
+		levelPercent: 100,
+	},
+	{
+		name: "Sass",
+		nameColor: "white",
+		bg: "#cf649a",
+		icon: <IoLogoSass />,
+		iconColor: "white",
+		gradientFrom: "#cf649a",
+		levelPercent: 90,
+	},
+	{
+		name: "Javascript",
+		nameColor: "white",
+		bg: "#ebc917",
+		icon: <IoLogoJavascript />,
+		iconColor: "white",
+		gradientFrom: "#ebc917",
+		levelPercent: 85,
+	},
+	{
 		name: "React",
-		nameColor: "text-white",
-		bg: "bg-[#222222]",
+		nameColor: "white",
+		bg: "#222222",
 		icon: <IoLogoReact />,
-		iconColor: "text-[#03d9fe]",
-		anchor: "/web",
+		iconColor: "#03d9fe",
+		gradientFrom: "#03d9fe",
+		levelPercent: 80,
+	},
+	{
+		name: "Next.js",
+		nameColor: "black",
+		bg: "#ffffff",
+		icon: <SiNextdotjs />,
+		iconColor: "black",
+		gradientFrom: "#ffffff",
+		levelPercent: 70,
+		outlined: true,
+	},
+	{
+		name: "Github",
+		nameColor: "white",
+		bg: "#000000",
+		icon: <IoLogoGithub />,
+		iconColor: "white",
+		gradientFrom: "#000000",
+		levelPercent: 80,
+	},
+	{
+		name: "Photoshop",
+		nameColor: "#41abe5",
+		bg: "#001e36",
+		icon: <SiAdobephotoshop />,
+		iconColor: "#41abe5",
+		gradientFrom: "#41abe5",
+		levelPercent: 60,
 	},
 ];
 
@@ -24,9 +107,9 @@ const About = () => {
 				<div className="flex items-center justify-evenly flex-col md:flex-row">
 					<div>
 						<h2 className="text-4xl font-bold leading-7">Who am I?</h2>
-						<p className="font-light text-silver my-3">
-							You might say that I'm a man of many <br />
-							<span className="underline text-white font-bold">
+						<p className="dark:text-silver text-blue-normal my-3">
+							You might say that I'm a dev of many <br />
+							<span className="underline dark:text-white text-black font-bold">
 								capabilities
 							</span>
 							. I do what the people need me to do!
@@ -39,11 +122,10 @@ const About = () => {
 						height="320"
 					/>
 				</div>
-				<p className="text-lg font-light">What do I do?</p>
-
+				<p className="text-lg font-light mt-4 md:mt-0">What are my skills?</p>
 				<section className="rounded-none">
 					<h3 className="font-bold text-2xl mb-4">I have experience with:</h3>
-					<div className="grid grid-cols-2 gap-2">
+					<div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
 						{expTech.map((tech, i) => {
 							return <TechCard key={i} tech={tech} />;
 						})}
