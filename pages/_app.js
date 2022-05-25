d97321be8706297b0e77a2c0c6d7f9a3afbbbf5d
orindholt <u75087@rts-365.dk>
2022-05-26 01:56:20 +0200

    Add favicon and manifest to head

--- pages/_app.js
+++ pages/_app.js
@@ -2,21 +2,44 @@ import { ThemeProvider } from "next-themes";
 import FormContextProvider from "../utils/Context";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+import Head from "next/head";
 
 const MyApp = ({ Component, pageProps, router }) => {
 	return (
-		<FormContextProvider>
-			<ThemeProvider
-				attribute="class"
-				defaultTheme="system"
-				enableSystem="true"
-				themes={["dark", "light"]}
-			>
-				<Layout router={router}>
-					<Component {...pageProps} />
-				</Layout>
-			</ThemeProvider>
-		</FormContextProvider>
+		<>
+			<Head>
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
+			</Head>
+			<FormContextProvider>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem="true"
+					themes={["dark", "light"]}
+				>
+					<Layout router={router}>
+						<Component {...pageProps} />
+					</Layout>
+				</ThemeProvider>
+			</FormContextProvider>
+		</>
 	);
 };
 
