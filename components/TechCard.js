f92aee7233652cb57606500b121e7c7063043e47
orindholt <u75087@rts-365.dk>
2022-05-26 01:47:44 +0200

    Add tech cards to about page

--- components/TechCard.js
+++ components/TechCard.js
@@ -1,24 +1,68 @@
+import { useState } from "react";
 import { IoAlert } from "react-icons/io5";
-import Link from "next/link";
+import { motion as m } from "framer-motion";
+import { shadeColor } from "../utils/shadeColor";
+
+const barLightenPercent = 20;
 
 const TechCard = ({ tech }) => {
 	const {
-		bg = "bg-blue-500",
+		bg = "blue",
 		icon = <IoAlert />,
-		iconColor = "text-white",
+		iconColor = "white",
 		name = "name",
-		nameColor = "text-white",
-		anchor = "/",
+		nameColor = "yellow",
+		gradientFrom = "blue",
+		gradientTo,
+		levelPercent = 50,
+		outlined = false,
 	} = tech;
+	const barVariant = {
+		hidden: { width: "0%" },
+		shown: {
+			width: `${levelPercent}%`,
+			transition: { duration: 1, delay: 0.5, type: "tween" },
+		},
+	};
+	const [active, setActive] = useState(false);
+
 	return (
-		<Link href={anchor}>
-			<li
-				className={`${bg} ${iconColor} w-full h-10 text-xl rounded-sm flex gap-1 items-center justify-center`}
+		<li
+			className="md:grid md:grid-rows-2 flex rounded-sm overflow-hidden border-4 border-solid"
+			style={{ borderColor: outlined ? "black" : bg }}
+			onClick={() => setActive(!active)}
+		>
+			<div
+				className={`max-w-[200px] md:max-w-none w-full h-10 text-xl flex gap-1 items-center justify-center md:pt-0.5`}
+				style={{ backgroundColor: bg, color: iconColor }}
 			>
 				{icon}
-				<h4 className={`${nameColor}`}>{name}</h4>
-			</li>
-		</Link>
+				<h4 className="font-medium" style={{ color: nameColor }}>
+					{name}
+				</h4>
+			</div>
+			<div
+				className="w-full border-solid border-2 md:border-4 flex p-1"
+				style={{
+					borderColor: bg,
+					backgroundColor: outlined ? "black" : "transparent",
+				}}
+			>
+				<m.div
+					variants={barVariant}
+					inital="hidden"
+					animate="shown"
+					className="h-full"
+					style={{
+						background: `linear-gradient(to right, ${gradientFrom}, ${
+							!gradientTo
+								? shadeColor(gradientFrom, barLightenPercent)
+								: gradientTo
+						})`,
+					}}
+				></m.div>
+			</div>
+		</li>
 	);
 };
 
