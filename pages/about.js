e160e6a7c1666ae6570062dbaac5f5f2d1542729
orindholt <u75087@rts-365.dk>
2022-05-26 19:00:29 +0200

    Move skills to seperate file

--- pages/about.js
+++ pages/about.js
@@ -1,101 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
 import TechCard from "../components/TechCard";
-import {
-	IoLogoReact,
-	IoLogoHtml5,
-	IoLogoCss3,
-	IoLogoJavascript,
-	IoLogoSass,
-	IoLogoGithub,
-} from "react-icons/io5";
-import { SiNextdotjs, SiTailwindcss, SiAdobephotoshop } from "react-icons/si";
-
-const expTech = [
-	{
-		name: "HTML5",
-		nameColor: "white",
-		bg: "#FF5733",
-		icon: <IoLogoHtml5 />,
-		iconColor: "white",
-		gradientFrom: "#FF5733",
-		levelPercent: 100,
-	},
-	{
-		name: "CSS3",
-		nameColor: "white",
-		bg: "#2196f3",
-		icon: <IoLogoCss3 />,
-		iconColor: "white",
-		gradientFrom: "#2196f3",
-		levelPercent: 90,
-	},
-	{
-		name: "Tailwind",
-		nameColor: "white",
-		bg: "#0bb6d4",
-		icon: <SiTailwindcss />,
-		iconColor: "white",
-		gradientFrom: "#0bb6d4",
-		gradientTo: "#1dc9b9",
-		levelPercent: 100,
-	},
-	{
-		name: "Sass",
-		nameColor: "white",
-		bg: "#cf649a",
-		icon: <IoLogoSass />,
-		iconColor: "white",
-		gradientFrom: "#cf649a",
-		levelPercent: 90,
-	},
-	{
-		name: "Javascript",
-		nameColor: "white",
-		bg: "#ebc917",
-		icon: <IoLogoJavascript />,
-		iconColor: "white",
-		gradientFrom: "#ebc917",
-		levelPercent: 85,
-	},
-	{
-		name: "React",
-		nameColor: "white",
-		bg: "#222222",
-		icon: <IoLogoReact />,
-		iconColor: "#03d9fe",
-		gradientFrom: "#03d9fe",
-		levelPercent: 80,
-	},
-	{
-		name: "Next.js",
-		nameColor: "black",
-		bg: "#ffffff",
-		icon: <SiNextdotjs />,
-		iconColor: "black",
-		gradientFrom: "#ffffff",
-		levelPercent: 70,
-		outlined: true,
-	},
-	{
-		name: "Github",
-		nameColor: "white",
-		bg: "#000000",
-		icon: <IoLogoGithub />,
-		iconColor: "white",
-		gradientFrom: "#000000",
-		levelPercent: 80,
-	},
-	{
-		name: "Photoshop",
-		nameColor: "#41abe5",
-		bg: "#001e36",
-		icon: <SiAdobephotoshop />,
-		iconColor: "#41abe5",
-		gradientFrom: "#41abe5",
-		levelPercent: 60,
-	},
-];
+import skills from "../utils/skills";
+import ReadMore from "../components/ReadMore";
 
 const About = () => {
 	return (
@@ -104,33 +11,64 @@ const About = () => {
 				<title>Oliver - About</title>
 			</Head>
 			<section>
-				<div className="flex items-center justify-evenly flex-col md:flex-row">
-					<div>
-						<h2 className="text-4xl font-bold leading-7">Who am I?</h2>
-						<p className="dark:text-silver text-blue-normal my-3">
-							You might say that I'm a dev of many <br />
-							<span className="underline dark:text-white text-black font-bold">
-								capabilities
-							</span>
-							. I do what the people need me to do!
-						</p>
+				<div className="flex items-center flex-col lg:flex-row md:px-24">
+					<div className="w-full mb-4">
+						<ReadMore
+							text={
+								<>
+									<h2 className="text-4xl font-bold leading-7 dark:text-white text-black">
+										Who am I?
+									</h2>
+									<p className="dark:text-silver text-blue-normal my-3">
+										You might say that I'm a dev of many <br />
+										<span className="underline dark:text-white text-black font-bold">
+											traits
+										</span>
+										. I do what the people need me to do!
+									</p>
+								</>
+							}
+							more={
+								<>
+									<p>
+										You name it, I can do it, or atleast google it.
+										<br />
+										I'm a quick learner, and really enjoy spending time coding.
+									</p>
+								</>
+							}
+						/>
 					</div>
 					<Image
 						className="rounded-full shadow-lg"
 						src="/assets/me.jpg"
-						width="320"
-						height="320"
+						width="400"
+						height="400"
+						quality={60}
+						placeholder="blur"
+						blurDataURL="/assets/placeholder.jpg"
 					/>
 				</div>
-				<p className="text-lg font-light mt-4 md:mt-0">What are my skills?</p>
-				<section className="rounded-none">
-					<h3 className="font-bold text-2xl mb-4">I have experience with:</h3>
-					<div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-						{expTech.map((tech, i) => {
-							return <TechCard key={i} tech={tech} />;
-						})}
-					</div>
-				</section>
+				<p className="text-2xl md:text-lg font-bold md:font-extralight mt-4 md:mt-0 mb-2 md:mb-0">
+					What are my skills?
+				</p>
+				<div className="flex flex-col gap-4 md:gap-6">
+					{skills.map(({ title, data }, i) => {
+						return (
+							<section key={i}>
+								<h3 className="font-semibold text-2xl mb-2 hidden md:block">
+									{title}:
+								</h3>
+								<div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+									{data.map((skill, i) => {
+										return <TechCard key={i} tech={skill} />;
+									})}
+								</div>
+							</section>
+						);
+					})}
+					<p>and much more to come!</p>
+				</div>
 			</section>
 		</>
 	);
