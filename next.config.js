7cb4dab54f6e0aa91080ddb3a84838ff407169ba
orindholt <U75087@rts-365.dk>
2022-09-20 13:18:56 +0200

    Modify next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const withPWA = require("next-pwa")({
 	dest: "public",
 	register: true,
 	skipWaiting: true,
+	disable: process.env.NODE_ENV === "development",
+	sw: "service-worker.js",
 });
 const nextConfig = {
 	reactStrictMode: true,
